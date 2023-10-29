c93a254e927caf3fe75f5332847cdbd7d3213c4e
Dmitry Misevra <shumi199@yandex.ru>
2023-10-29 17:31:50 +0600

    refactor: добавить в data.sql очистку таблиц

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,16 @@
+delete from user_friends;
+delete from film_likes;
+delete from film_genres;
+delete from users;
+delete from films;
+delete from feed;
+
+alter table feed alter column event_id restart with 1;
+alter table films alter column id restart with 1;
+alter table users alter column id restart with 1;
+
+
 insert into genres (id, name)
 values (1, 'Комедия'), (2,'Драма'), (3,'Мультфильм'), (4,'Триллер'), (5,'Документальный'), (6,'Боевик')
 on conflict do nothing;
+
