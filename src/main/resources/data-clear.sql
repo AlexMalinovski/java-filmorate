87ccc7c92555522d7e8f4ac2681a5f348ac29c57
Dmitry Misevra <shumi199@yandex.ru>
2023-10-30 00:25:59 +0600

    fix: исправить скрипты sql

--- src/main/resources/data-clear.sql
+++ src/main/resources/data-clear.sql
@@ -1,14 +1,16 @@
 delete from user_friends;
 delete from film_likes;
 delete from film_genres;
-delete from film_directors;
 delete from users;
 delete from films;
-delete from directors;
 delete from feed;
+delete from reviews;
+delete from review_reactions;
+delete from directors;
+delete from film_directors;
 
 alter table feed alter column event_id restart with 1;
 alter table films alter column id restart with 1;
 alter table users alter column id restart with 1;
-delete from reviews;
-delete from review_reactions;
+alter table directors alter column id restart with 1;
+alter table reviews alter column id restart with 1;
